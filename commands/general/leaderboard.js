eae1ef30e263a12b2f20150bf3af80f3c77b4f4b
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-04-09 17:32:02 -0500

    Fix leaderboard.js ???

--- commands/general/leaderboard.js
+++ commands/general/leaderboard.js
@@ -18,33 +18,32 @@ module.exports = {
             const startIndex = (page - 1) * 10;
             const endIndex = startIndex + 10;
             const pageUsers = userData.slice(startIndex, endIndex);
-        
+
             // Fetch user information from Discord for each user_id in pageUsers
             const leaderboardRowsPromises = pageUsers.map(async (user, index) => {
                 const rank = startIndex + index + 1;
                 const medal = rank === 1 ? '🥇' : rank === 2 ? '🥈' : rank === 3 ? '🥉' : '';
                 const rankText = `${medal}${rank}`.padEnd(5, ' ');
-        
+
                 // Fetch the user from Discord using their ID
                 const discordUser = await interaction.client.users.fetch(user.user_id).catch(console.error);
                 const userName = discordUser ? discordUser.username : 'Unknown User';
                 const trimmedName = (userName.length > 18 ? userName.substring(0, 15) + '...' : userName).padEnd(18, ' ');
                 const level = `Lv. ${user.level}`;
                 const xp = `${user.xp} XP`;
-        
+
                 return `\`${rankText}\` | ${trimmedName} | ${level} | ${xp}`;
             });
-        
+
             // Resolve all promises from the map
             const leaderboardRows = await Promise.all(leaderboardRowsPromises).then(rows => rows.join('\n'));
-        
+
             return new EmbedBuilder()
                 .setTitle(`${interaction.guild.name} Leaderboard`)
                 .setDescription(leaderboardRows)
                 .setFooter({ text: `Page ${page} of ${totalPages}` })
                 .setThumbnail(interaction.guild.iconURL());
         };
-        
 
         const updateComponents = (page, totalPages) => new ActionRowBuilder()
             .addComponents(
@@ -70,13 +69,15 @@ module.exports = {
         const collector = interaction.channel.createMessageComponentCollector({ filter, time: 120000 });
 
         collector.on('collect', async (i) => {
+            await i.deferUpdate();
+
             if (i.customId === 'previous_page' && currentPage > 1) {
                 currentPage--;
             } else if (i.customId === 'next_page' && currentPage < totalPages) {
                 currentPage++;
             }
 
-            await i.update({
+            await i.editReply({
                 embeds: [await generateEmbed(currentPage)],
                 components: [updateComponents(currentPage, totalPages)]
             });
