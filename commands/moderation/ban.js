abf4f0af2c822709d6b7cf702470cda28ee1b7a9
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-04-11 22:18:11 -0500

    Updated ban command

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -24,6 +24,9 @@ module.exports = {
         const reason = interaction.options.getString('reason');
 
         try {
+            // Attempt to message the user before banning
+            await user.send(`You have been banned from ${interaction.guild.name} for the following reason: ${reason}`).catch(error => console.error(`Could not send DM to ${user.username}:`, error));
+
             // Ban the user
             await interaction.guild.members.ban(user.id, { reason });
 
@@ -36,7 +39,7 @@ module.exports = {
                 date: new Date()
             });
 
-            await interaction.reply({ content: `${user.username} has been banned for the following reason: ${reason}` });
+            await interaction.reply({ content: `[${formattedTime}] 🔨 ${user.tag} (${user.id}) has been banned from the server by ${interaction.user.tag} for: ${reason}.` });
         } catch (error) {
             console.error(error);
             return interaction.reply({ content: "Failed to ban the user. They might have a higher role than me or I lack the permission to ban them.", ephemeral: true });
