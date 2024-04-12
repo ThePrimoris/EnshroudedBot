f1314ce1fe17dcb5c962e59f899c3eff18104cbc
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-04-11 23:04:54 -0500

    Formatting on unban

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -22,10 +22,12 @@ module.exports = {
 
         const userId = interaction.options.getString('user_id');
         const reason = interaction.options.getString('reason'); // Reason is now required
+        const currentTime = new Date();
+        const formattedTime = currentTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
 
         try {
             await interaction.guild.members.unban(userId, { reason });
-            await interaction.reply({ content: `User with ID: \`${userId}\` has been unbanned by <@${interaction.user.id}> for the following reason: \`${reason}\`.` }); // Notification is visible to everyone
+            await interaction.reply({ content: `\`[${formattedTime}]\` User with ID: \`${userId}\` has been unbanned by <@${interaction.user.id}> for the following reason: \`${reason}\`.` }); // Notification is visible to everyone
         } catch (error) {
             console.error(error);
             return interaction.reply({ content: "Failed to unban the user. They might not be banned, or I might lack the permission to unban them.", ephemeral: true });
