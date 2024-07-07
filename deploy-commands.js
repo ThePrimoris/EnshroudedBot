a375edbe78d5171bb4d82aed71788ff63ca3ab8b
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-07-07 08:35:22 -0500

    Updated Deploy Commands
    Changed directory to new slash-commands directory to make way for prefix commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,21 +11,27 @@ const commands = [];
 const commandFolders = ['general', 'moderation'];
 
 for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(path.join(__dirname, 'commands', folder)).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        // Check if the command.data is an instance of SlashCommandBuilder
-        if (command.data && command.data.toJSON) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.error(`The command at './commands/${folder}/${file}' does not properly export a SlashCommandBuilder instance.`);
+    const commandsPath = path.join(__dirname, 'commands', 'slash-commands', folder);
+    
+    // Check if the directory exists
+    if (fs.existsSync(commandsPath)) {
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./commands/slash-commands/${folder}/${file}`);
+            // Check if the command.data is an instance of SlashCommandBuilder
+            if (command.data && command.data.toJSON) {
+                commands.push(command.data.toJSON());
+            } else {
+                console.error(`The command at './commands/slash-commands/${folder}/${file}' does not properly export a SlashCommandBuilder instance.`);
+            }
         }
+    } else {
+        console.warn(`Directory not found: ${commandsPath}`);
     }
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 console.log(JSON.stringify(commands, null, 2)); // This will print the commands being registered in a readable format
-
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
