07e8a1f7c16aba646736a21894d6df59e6f81fd5
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-07-07 15:59:56 -0500

    checking if portal knights still works

--- events/portalKnights.js
+++ events/portalKnights.js
@@ -1,19 +1,17 @@
 const { Events } = require('discord.js');
 
-const specificUserId = '575756653090308136'; // User ID to scan for
+const specificUserId = '1225491801322946675'; // User ID to scan for
 
 module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
         // Check if the message is from the specific user, contains "portal knights", and is not from a bot
-        if (message.author.id === specificUserId && !message.author.bot && message.content.toLowerCase().includes('portal knights')) {
+        if (message.author.id === specificUserId && !message.author.bot && message.content.toLowerCase().includes('test')) {
             // Reply to the user
-            await message.channel.send("Did someone say Portal Knights!?");
+            await message.channel.send("Test received!");
 
             // React to the message with :regional_indicator_o:, :regional_indicator_m:, :regional_indicator_g:
-            await message.react('🇴');
-            await message.react('🇲');
-            await message.react('🇬');
+            await message.react('T');
         }
     },
 };
