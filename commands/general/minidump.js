ff6713fa0caef4d5d8132f7fd64767006eb3af92
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-08-01 23:54:18 -0500

    Added line breaks

--- commands/general/minidump.js
+++ commands/general/minidump.js
@@ -11,12 +11,18 @@ module.exports = {
         **Step 1: Verify game files via Steam**
         - How to verify your game files on Steam: [Click Here](https://help.steampowered.com/en/faqs/view/0C48-FCBD-DA71-93EB)
         - Check to see if this resolved the issue.
+
+
         **Step 2: Use Liandry's Tool**
         - Check the README on the site on how to use it. [Enshrouded Tool Fix](https://github.com/LiaNdrY/Enshrouded-Tool-Fix)
         - Check to see if this resolved the issue.
+
+
         **Step 3: Rename enshrouded_local.json**
         - Rename the fole located here: > "...\Steam\steamapps\common\Enshrouded\enshrouded_local.json" (will reset ingame settings)
         - Check to see if this resolved the issue.
+
+        
         **Step 4: Clean reinstall GPU driver (with DDU)**
         - [Clean install with DDU](https://www.youtube.com/watch?v=v7KfnZ2wSog)
         `.trim();
