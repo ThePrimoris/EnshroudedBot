a6d46a5245d2967031bcc638c8ea54d63cc182c3
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-08-02 01:03:00 -0500

    correct link to altar image

--- commands/general/altars.js
+++ commands/general/altars.js
@@ -27,7 +27,7 @@ Any structures or items created by the player within the flame radius remain for
             .setTitle('How do Flame Altars & Flame Levels work?')
             .setDescription(embedDescription)
             .setColor('#3f9dd5')
-            .setThumbnail('https://enshrouded.wiki.gg/wiki/File:Flame_Altar.png')
+            .setThumbnail('https://enshrouded.wiki.gg/images/9/96/Flame_Altar.png')
             .setFooter({ text: embedFooter.trim() });
 
         await interaction.reply({ embeds: [embed] });
