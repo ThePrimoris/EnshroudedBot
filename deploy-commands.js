345ce318e9e850b0d46dcdc92da3a3308bbd6873
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-09-11 22:34:17 -0500

    updated deploy-commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,37 +6,33 @@ const token = process.env.DISCORD_BOT_TOKEN;
 const fs = require('fs');
 const path = require('path');
 
-// Array to hold commands (both slash and context menu)
 const commands = [];
-const commandFolders = ['general', 'moderation'];
+const commandFolders = ['general', 'moderation', 'context'];
 
 for (const folder of commandFolders) {
     const commandFiles = fs.readdirSync(path.join(__dirname, 'commands', folder)).filter(file => file.endsWith('.js'));
-    
     for (const file of commandFiles) {
         const command = require(`./commands/${folder}/${file}`);
-        
-        // Handle if command.data is an array (to handle multiple commands per file, if needed)
+        // Check if command.data is an array (for multiple command types) or a single instance
         if (Array.isArray(command.data)) {
-            command.data.forEach((cmd) => {
-                if (cmd && cmd.toJSON) {
-                    commands.push(cmd.toJSON());
+            for (const data of command.data) {
+                if (data && data.toJSON) {
+                    commands.push(data.toJSON());
                 } else {
-                    console.error(`The command at './commands/${folder}/${file}' does not properly export a command instance.`);
+                    console.error(`The command at './commands/${folder}/${file}' does not properly export a valid command instance.`);
                 }
-            });
+            }
         } else if (command.data && command.data.toJSON) {
-            // Push to commands if it has a .toJSON() method (this works for both slash and context menu commands)
             commands.push(command.data.toJSON());
         } else {
-            console.error(`The command at './commands/${folder}/${file}' does not properly export a command instance.`);
+            console.error(`The command at './commands/${folder}/${file}' does not properly export a valid command instance.`);
         }
     }
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-console.log(JSON.stringify(commands, null, 2)); // Print commands being registered
+console.log(JSON.stringify(commands, null, 2)); // This will print the commands being registered in a readable format
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
