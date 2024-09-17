ae0cdba54f4f7854f2c5ba855aefee560c027a72
Matthew <30489975+ThePrimoris@users.noreply.github.com>
2024-09-17 16:14:40 -0500

    testing roadmap

--- commands/general/roadmap.js
+++ commands/general/roadmap.js
@@ -7,11 +7,14 @@ module.exports = {
     async execute(interaction) {
         const embed = new EmbedBuilder()
             .setTitle('Enshrouded Roadmap')
-            .setDescription('Here is the current roadmap for Enshrouded.')
+            .setDescription('The current roadmap for Enshrouded.')
             .setColor('#3f9dd5')
+            .setThumbnail('https://i.imgur.com/Cah6itb.png')
+            .addFields(
+                { name: 'Test', value: 'Here are some words for you.', inline: true }
+            )
             .setImage('https://i.imgur.com/KEd5DIx.jpeg') // Replace with your image URL
             .setFooter({ text: 'Roadmap current as of: 13 March 2024' }) // Customize footer
-            .setTimestamp(); // Optional: shows when the embed was created
 
         await interaction.reply({ embeds: [embed], ephemeral: true });
     },
